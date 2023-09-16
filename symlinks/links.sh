84c00cf198f8108b9944ee413b0a8733de545ab1
Samir Rodas <serodas9710@gmail.com>
2023-09-16 16:50:41 -0500

    Incluye enlace simbolico de aliases.zsh

--- symlinks/links.sh
+++ symlinks/links.sh
@@ -3,3 +3,4 @@ ln -s .dotfiles/.gitconfig $PWD/.gitconfig
 ln -s .dotfiles/.gitignore_global $PWD/.gitignore_global
 ln -s .dotfiles/.profile $PWD/.profile
 ln -s .dotfiles/.zshrc $PWD/.zshrc
+ln -s .dotfiles/.oh-my-zsh/custom/aliases.zsh $PWD/.oh-my-zsh/custom/aliases.zsh
