6e1683cf9ef483f38e155b1bbf75032056530f24
Samir Rodas <serodas9710@gmail.com>
2023-09-16 12:24:10 -0500

    Agrega links simbolicos

---
+++ symlinks/links.sh
@@ -0,0 +1,5 @@
+ln -s .dotfiles/.bashrc $PWD/.bashrc
+ln -s .dotfiles/.gitconfig $PWD/.gitconfig
+ln -s .dotfiles/.gitignore_global $PWD/.gitignore_global
+ln -s .dotfiles/.profile $PWD/.profile
+ln -s .dotfiles/.zshrc $PWD/.zshrc
